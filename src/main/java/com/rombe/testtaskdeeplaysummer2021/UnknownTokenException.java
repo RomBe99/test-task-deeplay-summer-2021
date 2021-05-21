e537d8bdf299a5e0941ecfba940c9ab8d9e693e2
Belinsky Roman <romanbelinsky99@gmail.com>
2021-05-21 16:20:37 +0600

    Update constructor UnknownTokenException

--- src/main/java/com/rombe/testtaskdeeplaysummer2021/UnknownTokenException.java
+++ src/main/java/com/rombe/testtaskdeeplaysummer2021/UnknownTokenException.java
@@ -4,7 +4,7 @@ public class UnknownTokenException extends RuntimeException {
     private final String unknownToken;
 
     public UnknownTokenException(String unknownToken) {
-        super("Unknown token: ".concat(unknownToken));
+        super(String.format("Unknown token: %s", unknownToken));
 
         this.unknownToken = unknownToken;
     }
